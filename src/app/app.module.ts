9363fb5d123ce3c3e3e2b4239154459dcfad47d9
SeregeySulimenko <sergey-sulimenko@mail.ru>
2021-02-19 18:34:36 +0400

    init angular and nebular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NbThemeModule, NbLayoutModule, NbSidebarModule} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({name: 'cosmic'}),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    AppRoutingModule,
+    NbSidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
